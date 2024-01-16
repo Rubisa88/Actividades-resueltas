60632058c0b8c42e62951b318a0efbb173429518
Rubisa88 <156830614+Rubisa88@users.noreply.github.com>
2024-01-16 20:15:18 +0100

    Add files via upload

---
+++ Actividad1_2.java
@@ -0,0 +1,19 @@
+package unidad1;
+
+import java.util.Scanner;
+
+public class Actividad1_2 {
+    public static void main(String[] args) {
+
+        System.out.println("Inserte un número entero");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println( "Valor introducido: " + num);
+
+
+
+    }
+
+
+
+}
