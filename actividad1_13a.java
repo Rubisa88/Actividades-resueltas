e16accf5354fcd408832d595cdd4eb4873ad0f5f
Rubisa88 <156830614+Rubisa88@users.noreply.github.com>
2024-01-25 18:55:07 +0100

    Add files via upload

---
+++ actividad1_13a.java
@@ -0,0 +1,22 @@
+package unidad1;
+//como la acrtividad 12 pero con dos numeros
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class actividad1_13a {
+    public static void main(String[] args) {
+        int num, num2, multiplo;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Ingrese un número:");
+        num= sc.nextInt();
+        System.out.println("Ingrese otro número: ");
+        num2= sc.nextInt();
+        multiplo= num2-num%num2;
+        System.out.println("Al número " + num +  " hay que sumarle "+ multiplo + " para que sea multiplo de "+ num2 );
+
+
+
+    }
+
+}
